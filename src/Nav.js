244620bb012e3d0c30a3e1ab4316f2a3ecfcc818
khaly ruot <khalyruot12345@gmail.com>
2021-02-26 02:26:14 -0500

    deploy

---
+++ src/Nav.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './App.css';
+
+
+
+function Nav() {
+  return (
+    <div className="button">
+             
+            
+            <h1 className="item1">Hello</h1>
+           
+            
+            <button className="name">
+                1
+            </button>
+            <button className="name">
+                2
+            </button>
+            <button className="name">
+                3
+            </button>
+            <button className="name">
+                4
+            </button>
+            <button className="name">
+                5
+            </button>
+            <button className="name">
+                6
+            </button>
+            <button className="name">
+                7
+            </button>
+            <button className="name">
+                8
+            </button>
+            <button className="name">
+                9
+            </button>
+            <button className="name">
+                +
+            </button>
+            <button className="name">
+                x
+            </button>
+            <button className="name">
+                =
+            </button>
+            <button className="name">
+                -
+            </button>
+            <button className="name">
+                /
+            </button>
+            <button className="name">
+                c
+            </button>
+            
+
+        </div>
+  );
+}
+
+export default Nav;
