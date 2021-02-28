e831627bcaee864a11990bd964d3b08a9e777b8d
khaly ruot <khalyruot12345@gmail.com>
2021-02-28 01:08:22 -0500

    update

--- src/App.js
+++ src/App.js
@@ -5,11 +5,57 @@ import Nav from './Nav';
 
 
 function App() {
+    
   return (
-    <div>
+    <div className="button">
       
       <Nav />
-  
+      <button className="name">
+                1
+            </button>
+            <button className="name">
+                2
+            </button>
+            <button className="name">
+                3
+            </button>
+            <button className="name">
+                4
+            </button>
+            <button className="name">
+                5
+            </button>
+            <button className="name">
+                6
+            </button>
+            <button className="name">
+                7
+            </button>
+            <button className="name">
+                8
+            </button>
+            <button className="name">
+                9
+            </button>
+            <button className="name">
+                +
+            </button>
+            <button className="name">
+                x
+            </button>
+            <button className="name">
+                =
+            </button>
+            <button className="name">
+                -
+            </button>
+            <button className="name">
+                /
+            </button>
+            <button className="name">
+                c
+            </button>
+            
     </div>
   );
 }
